e13589136cb51b3d1a2c129716381d56c60bac70
dennismutitu <dennismutitu06@gmail.com>
2024-07-11 12:08:16 +0300

    redirect with a bug

--- redirect.py
+++ redirect.py
@@ -1,14 +1,18 @@
 #to redirect user to other endpoint use the redirect() func
 #to abort a req early with an error code use the abort() func
 
-from flask import abort, redirect, url_for
+from flask import abort, redirect, url_for, Flask
 
-@app.route('/')
+redirect = Flask(__name__)
+
+@redirect.route('/')
 def index():
     return redirect(url_for('login')) #url_for gen the url for login route
 
-@app.route('/login')
+@redirect.route('/login')
 def login():
     abort(401)
     this_is_never_executed() #this func will not b exec coz abort terminates
 
+if __name__ == '__main__':
+    redirect.run(debug=True)
